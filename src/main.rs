003b239d92fe5162739968669a0885d78939de33
Vassiliy Kuzenkov <vasiliy.kuzenkov@dualbootpartners.com>
2023-03-24 23:59:22 +0600

    implement file isolation

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,62 @@
-use libc::{c_char, chroot};
-use std::{io::Write, path::Path};
+use std::env::{args, set_current_dir};
+use std::fs::{copy, create_dir, create_dir_all, set_permissions, File, Permissions};
+use std::os::unix::fs::{chroot, PermissionsExt};
+use std::process::{exit, Command, Stdio};
 
 use anyhow::{Context, Result};
+use tempfile::TempDir;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
+    let args: Vec<_> = args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    let exit_code = run_command(command, command_args)?;
+    exit(exit_code);
+}
+
+fn run_command(command: &String, command_args: &[String]) -> Result<i32> {
+    let temp_dir = tempfile::tempdir()?;
+
+    copy_command(command, &temp_dir)?;
+
+    create_dev_null(&temp_dir)?;
+
+    chroot(temp_dir.path())?;
+
+    set_current_dir("/")?;
+
+    let mut command = Command::new(command)
         .args(command_args)
-        .output()
+        .stdin(Stdio::null())
+        .spawn()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
                 command, command_args
             )
         })?;
+    Ok(command.wait()?.code().unwrap_or(1))
+}
 
-    let temp_dir = tempfile::tempdir().with_context(|| "Failed to create temp dir")?;
-    let temp_dir_path = temp_dir.path();
-    let tmp_dir_path_cchar = temp_dir_path
-        .to_str()
-        .with_context(|| "Failed to convert temp dir path to c_char")?
-        .as_ptr() as *const c_char;
+fn copy_command(command: &String, temp_dir: &TempDir) -> Result<()> {
+    let command_path_relative = command.trim_start_matches("/");
+    let target_command = temp_dir.path().join(command_path_relative);
+    let target_path = target_command.parent().unwrap();
 
-    let command_path = Path::new(command);
-
-    copy_executable_into_dir(temp_dir_path, command_path)?;
-    create_dev_null(temp_dir_path)?;
-
-    unsafe {
-        chroot(tmp_dir_path_cchar);
-    }
-
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        let std_err = std::str::from_utf8(&output.stderr)?;
-
-        std::io::stdout().write_all(std_out.as_bytes())?;
-        std::io::stderr().write_all(std_err.as_bytes())?;
-    } else {
-        let exit_code = output.status.code().unwrap_or(1);
-        std::process::exit(exit_code);
-    }
+    create_dir_all(target_path)?;
+    copy(command, target_command)?;
 
     Ok(())
 }
 
-fn copy_executable_into_dir(chroot_dir: &Path, executable_path: &Path) -> Result<()> {
-    let executable_name = executable_path
-        .file_name()
-        .with_context(|| "Failed to get executable name")?
-        .to_str()
-        .with_context(|| "Failed to convert executable name to str")?;
+fn create_dev_null(temp_dir: &TempDir) -> Result<()> {
+    create_dir(temp_dir.path().join("dev"))?;
+    set_permissions(temp_dir.path().join("dev"), Permissions::from_mode(0o555))?;
 
-    let chroot_executable_path = chroot_dir.join(executable_name);
-    std::fs::copy(executable_path, chroot_executable_path)
-        .with_context(|| "Failed to copy executable into chroot dir")?;
-
-    Ok(())
-}
-
-fn create_dev_null(chroot_dir: &Path) -> Result<()> {
-    let dev_null_path = chroot_dir.join("dev").join("null");
-    std::fs::create_dir_all(dev_null_path)
-        .with_context(|| "Failed to create /dev/null in chroot dir")?;
+    File::create(temp_dir.path().join("dev/null"))?;
+    set_permissions(temp_dir.path().join("dev/null"), Permissions::from_mode(0o555))?;
 
     Ok(())
 }
